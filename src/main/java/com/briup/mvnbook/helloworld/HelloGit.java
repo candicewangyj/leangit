242e1fd5f269a681bed67eddf4b83dc9b5e1d296
candice <candice@192.168.0.129>
2018-02-26 15:25:52 +0800

    test git add test(String)

--- src/main/java/com/briup/mvnbook/helloworld/HelloGit.java
+++ src/main/java/com/briup/mvnbook/helloworld/HelloGit.java
@@ -4,6 +4,9 @@ public class HelloGit {
 	public void test(){
 		System.out.println("hello wolrd");
 	}
+	public void test(String test){
+		System.out.println(test);
+	}
 	public static void main(String[] args) {
 		System.out.println("hello git");
 		
